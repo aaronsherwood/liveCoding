ef12e85ced807ea26886aedfe8ff4e9b2ea80f8f
aaronsherwood <info@aaron-sherwood.com>
2022-10-01 18:32:07 +0400

    more triangles

--- Class_Examples/10_week7_trigger_hydra_scripts.js
+++ Class_Examples/10_week7_trigger_hydra_scripts.js
@@ -3,14 +3,21 @@ shape(3).blend(osc(),1).out()
 
 // make an array of hydra scripts
 visuals = [
-  ()=>shape(3,0.3,0.025).scale(1,innerHeight/innerWidth).out(),
+  ()=>{shape(3,0.3,0.025).scale(1,innerHeight/innerWidth).out(o0)},
   ()=>shape(3,0.3,0.025).scale(1,innerHeight/innerWidth).invert().out(),
   ()=>shape(3,0.3,0.025).scale(1,innerHeight/innerWidth).invert().add(osc(30,0.01,0.5)).out(),
   ()=>{src(s0)
-    .blend(src(o0).diff(s0).scale(.9999)
-  	.modulatePixelate(noise(10,0.001).pixelate(16,16),1024),1)
+    .blend(src(o0).diff(s0).scale(.999)
+  	.modulatePixelate(noise(10,0.01).pixelate(16,16),1024),1)
     .out()},
-  ()=>shape(3,0.3,0.1).scale(1,innerHeight/innerWidth).invert().add(osc(30,0.01,0.5)).modulate(noise(3,0.05),.03).out()
+  ()=>{
+  	shape(3,0.3,0.1).scale(1,innerHeight/innerWidth).invert().add(osc(30,0.01,0.5)).modulate(noise(3,0.05),.03).out(o1)
+    src(o0)
+      .modulate(
+        osc(6,0,1.5).modulate(noise(3),1).brightness(-0.5)
+      ,0.003)
+      .blend(o1,0.005).out()
+      }
 ]
 
 visuals[0]()
