e1285fa992880c11e488c24e3dc24193bd23bbac
aaronsherwood <info@aaron-sherwood.com>
2022-10-01 12:52:21 +0400

    more video stuff

--- Class_Examples/08_week6_hydra_video.js
+++ Class_Examples/08_week6_hydra_video.js
@@ -1,19 +1,21 @@
-// create an html5 video element
-vid = document.createElement('video')
-vid.autoplay = true
-vid.loop = true
-
 // an array for the video names, use complete file path
 // you need to change the base path (and names) for your own videos!
 basePath = "/Users/ags419/Documents/Code/classes/liveCoding/media/"
-videos = [basePath+"0.mp4", basePath+"1.mp4", basePath+"2.mp4", basePath+"3.mp4", basePath+"4.mp4"]
+// basePath = ""https://blog.livecoding.nyuadim.com/wp-content/uploads/"" // to use with our class flok server
+videoNames = [basePath+"0.mp4", basePath+"1.mp4", basePath+"2.mp4", basePath+"3.mp4", basePath+"4.mp4"] //"https://media2.giphy.com/media/ny7UCd6JETnmE/giphy.mp4"
+vids = []
+for (let i=0; i<videoNames.length; i++){
+	vids[i] = document.createElement('video')
+	vids[i].autoplay = true
+	vids[i].loop = true
+  vids[i].crossOrigin="anonymous"
+  vids[i].src = videoNames[i]
+}
 
 // choose video source from array
 whichVid = 4
-vid.src = videos[whichVid]
-
-// use video within hydra
-s0.init({src: vid})
+// use video within hydra (we can also use initVideo and then jsut the filepath, but this will add a video element every time: s0.initVideo("videoFilePathAndName") )
+s0.init({src: vids[whichVid]})
 
 // main function, blending a secondary function controlled by cc[1] in
 src(s0)
@@ -29,7 +31,7 @@ update = () =>{
   // very important! only change source once, when necessary
   if (whichVid != ccActual[0]){
     whichVid = ccActual[0];
-    vid.src = videos[whichVid]
+    s0.init({src: vids[whichVid]})
   }
 }
 
