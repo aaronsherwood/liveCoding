ef5aff0e3e382571bf01c2edc63ec02508e7afa9
aaronsherwood <info@aaron-sherwood.com>
2022-10-01 21:08:19 +0400

    week 8

--- Class_Examples/13_week8_p5j.js
+++ Class_Examples/13_week8_p5j.js
@@ -70,68 +70,20 @@ videos = [basePath+"0.mp4", basePath+"1.mp4", basePath+"2.mp4", basePath+"3.mp4"
 
 // choose video source from array
 vid.src = videos[0]
-
 // use video within hydra
 s1.init({src: vid})
+
 // cc[1] will be zero always during the "A" section
 // thus making the scale value not change
 src(s1).scale(()=>-1*cc[0]*cc[1]+1).out()
 render(o0)
-p5 = new P5()
-s0.init({src: p5.canvas})
-let prev = -1;
-p5.draw = ()=>{
-  p5.background(0);
+let whichVideo = -1;
+update = ()=> {
   // only change the video when the number from tidal changes,
   // otherwise the video wil keep triggering from the beginning and look like it's not playing
   // cc[2] is for changing the video
-  if (cc[2]!=prev){
-    // cc[1] changes when it is the "B" section
-    if (cc[1]==1){
-      if (cc[2]==0){
-        vid.src = videos[1]
-      } else if (cc[2]==1){
-        vid.src = videos[4]
-      }
-    } else {
-      if (cc[2]==0){
-        vid.src = videos[0]
-      } else if (cc[2]==1){
-        vid.src = videos[3]
-      }
-    }
-    prev=cc[2];
+  if (cc[2]!=whichVideo){
+    vid.src = videos[ccActual[2]]
+    whichVideo=cc[2];
   }
 }
-
-///////////////////////////////////////////////////////////////////////
-// register WebMIDI; need to paste this in console
-// access console cmnd + optn + i (macOS)
-// cntl + shft + i (windows)
-navigator.requestMIDIAccess()
-    .then(onMIDISuccess, onMIDIFailure);
-
-function onMIDISuccess(midiAccess) {
-    console.log(midiAccess);
-    var inputs = midiAccess.inputs;
-    var outputs = midiAccess.outputs;
-    for (var input of midiAccess.inputs.values()){
-        input.onmidimessage = getMIDIMessage;
-    }
-}
-
-function onMIDIFailure() {
-    console.log('Could not access your MIDI devices.');
-}
-
-//create an array to hold our cc values and init to a normalized value
-var cc=Array(128).fill(0.5)
-
-getMIDIMessage = function(midiMessage) {
-    var arr = midiMessage.data
-    var index = arr[1]
-    //console.log('Midi received on cc#' + index + ' value:' + arr[2])    // uncomment to monitor incoming Midi
-    var val = (arr[2])/127.0  // normalize CC values to 0.0 - 1.0
-    cc[index]=val
-}
-///////////////////////////////////////////////////////////////////////
